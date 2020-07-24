c2a5e47b8e067009d5ec076e19cb22c1d0e22afd
hoshi.fukuyama <debsen17xxx@icloud.com>
2020-07-24 14:15:09 +0900

    Initial Commit

---
+++ MetalTest4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalTest4
+//
+//  Created by 福山帆士 on 2020/07/24.
+//  Copyright © 2020 福山帆士. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
